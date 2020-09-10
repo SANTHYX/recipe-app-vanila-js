973e607fe193aef308a37ee9b914a19eed935c8a
Damian <50864003+SANTHYX@users.noreply.github.com>
2020-09-10 16:50:09 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/scripts/main.js',
+	devtool: 'eval-cheap-module-source-map',
+	output: {
+		filename: './scripts/main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].css',
+							outputPath: 'styles/'
+						}
+					},
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader?-url'
+					},
+					{
+						loader: 'postcss-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			},
+			{
+				test: /\.html$/,
+				use: ['html-loader']
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'img/'
+						}
+					}
+				]
+			}
+		]
+	}
+};
